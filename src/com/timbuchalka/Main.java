3da4570f396b129df208db07aa998bf6a1da639e
bieradek <radoslaw.biedrzycki@syncron.com>
2018-08-18 23:51:50 +0200

    Uploading some code

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -3,6 +3,17 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
+        Player player1 = new Player();
+        player1.name = "Tim";
+        player1.health = 100;
+        player1.weapon = "Hatchet";
 
+        int damage = 10;
+        player1.loseHealth(damage);
+        System.out.println("Remaining health " + player1.healthRemaining());
+
+        damage = 11;
+        player1.loseHealth(damage);
+        System.out.println("Remaining health " + player1.healthRemaining());
     }
 }
